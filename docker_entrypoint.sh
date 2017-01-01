da43b1e6a08832728637abe3944bdd1419127291
Thomas M Hughes <hughes.thomas.m@gmail.com>
2017-01-01 05:54:20 -0500

    feat: Use gunicorn instead of 'runserver'

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -2,4 +2,20 @@
 export PYTHONPATH=/usr/local/lib/python2.7/site-packages
 
 /usr/bin/python /code/acp/manage.py migrate
-/usr/bin/python /code/acp/manage.py runserver 0.0.0.0:8000
+/usr/bin/python /code/acp/manage.py collectstatic --noinput  # Collect static files
+
+# Prepare log files and start outputting logs to stdout
+touch /srv/logs/gunicorn.log
+touch /srv/logs/access.log
+tail -n 0 -f /srv/logs/*.log &
+
+# Start Gunicorn processes
+echo Starting Gunicorn.
+exec gunicorn acp.wsgi:application \
+    --name acp \
+    --bind 0.0.0.0:8000 \
+    --workers 3 \
+    --log-level=info \
+    --log-file=/srv/logs/gunicorn.log \
+    --access-logfile=/srv/logs/access.log \
+    "$@"
