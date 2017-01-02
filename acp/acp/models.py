8366dd074d4f73b904e581f28d0446387ca4269f
Thomas M Hughes <hughes.thomas.m@gmail.com>
2017-01-02 11:41:34 -0500

    feat: Add model framework for Corpus, Category and Document

---
+++ acp/acp/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from django.conf import settings
+
+class Corpus(models.Model):
+    """ A collection of categories and documents that will be clustered """
+    name = models.CharField(max_length=64) # Name of the Corpus
+    public = models.BooleanField(default=False)
+
+class CorpusOwners(models.Model):
+    """ More than one person can own a Corpus; this gives them editing rights """
+    corpus = models.ForeignKey('Corpus')
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL)
+    
+
+class Category(models.Model):
+    """ A sub-category attached to a corpus (often an author) """
+    name = models.CharField(max_length=64)
+    corpus = models.ForeignKey('Corpus')
+    color = models.CharField(max_length=64)
+    mark = models.CharField(max_length=64)
+    labels = models.BooleanField(default=False)
+
+class Document(models.Model):
+    """ Specific document metadata reference.  The actual document is stored 
+    remotely.  """
+    name = models.CharField(max_length=64)
+    corpus = models.ForeignKey('Corpus')
+    category = models.ForeignKey('Category')
+    uri = models.URLField()
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL)
+    
--- acp/acp/settings.py
+++ acp/acp/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'social_auth',
+    'acp',
 )
 
 # Social Authenticators
