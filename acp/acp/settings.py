c19902b9ff4fb055d21861117e6ccf93b3ad16e9
Thomas M Hughes <hughes.thomas.m@gmail.com>
2017-01-19 08:28:35 -0500

    feat: File uploads now stored in Amazon S3

--- acp/acp/settings.py
+++ acp/acp/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = (
     'allauth.account',
     'allauth.socialaccount',
     'allauth.socialaccount.providers.google',
+    'storages',
     'acp',
 )
 
@@ -134,4 +135,11 @@ DATABASES = {
 GOOGLE_OAUTH2_CLIENT_ID      = os.environ.get('GOOGLE_OAUTH2_CLIENT_ID', '')
 GOOGLE_OAUTH2_CLIENT_SECRET  = os.environ.get('GOOGLE_OAUTH2_CLIENT_SECRET', '')
 
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID', '')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY', '')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME', '')
+AWS_S3_FILE_OVERWRITE = False
+AWS_LOCATION = 'corpa'
+AWS_S3_CALLING_FORMAT='boto.s3.connection.OrdinaryCallingFormat'
