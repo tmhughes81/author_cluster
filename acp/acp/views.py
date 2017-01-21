db12f9072394f2d828039831eca4076dbadc4c33
Thomas M Hughes <hughes.thomas.m@gmail.com>
2017-01-21 08:26:14 -0500

    feat: Update category and document counts on creation/deletion

--- acp/acp/views.py
+++ acp/acp/views.py
@@ -5,6 +5,7 @@ from forms import CreateCorpusForm, AddDocForm, AddCatForm
 from models import Corpus, CorpusOwners, Category, Document, Visual
 from django.contrib.auth.models import User
 from cluster import create_visual
+from django.db.models import Q
 from django.utils.datastructures import MultiValueDictKeyError
 
 def index(request):
@@ -12,13 +13,24 @@ def index(request):
     return render(request, "index.html", args)
 
 def dashboard(request):
-
+    """ Lists all available corpa to a user """
     args = {}
     
-    corpa = Corpus.objects.all()
+    # First list the public corpa
+    try:
+        public_corpa = Corpus.objects.filter(public=True)
+        args.update({'public_corpa': public_corpa})
+    except:
+        pass
     
-    args.update({'corpa': corpa})
+    # Then try the user's corpa; double list public corpa
+    try:
+        my_corpa_owned = CorpusOwners.objects.filter(owner=request.user)
+        args.update({'my_corpa_owned': my_corpa_owned})
+    except CorpusOwners.DoesNotExist:
+        pass
     
+    # Add a form for creating new corpa
     form = CreateCorpusForm()
     
     args.update({'form': form})
@@ -123,6 +135,9 @@ def add_doc(request, corpus_id):
             doc.corpus = Corpus.objects.get(id=corpus_id)
             doc.category = Category.objects.get(name=request.POST['category'])
             doc.save()
+            
+            doc.corpus.doc_count = Document.objects.filter(corpus=doc.corpus).count()
+            doc.corpus.save()
     else:
         HttpResponseRedirect('/')
     
@@ -151,6 +166,10 @@ def add_cat(request, corpus_id):
             cat_form = form.save(commit=False)
             cat_form.corpus = Corpus.objects.get(id=corpus_id)
             cat_form.save()
+            
+            cat_form.corpus.cat_count = Document.objects.filter(corpus=cat_form.corpus).count()
+            cat_form.corpus.save()
+            
     else:
         HttpResponseRedirect('/')
     
@@ -178,8 +197,12 @@ def del_doc(request, doc_id):
     if not Document.objects.filter(id=doc_id).exists():
         return HttpResponseRedirect('/corpus/not_found/')
     
-    
     doc.delete()
+    
+    # Update the document count
+    corpus.doc_count = Document.objects.filter(corpus=corpus).count()
+    corpus.save()
+
     
     return HttpResponseRedirect('/corpus/'+str(corpus.id))
 
@@ -200,6 +223,11 @@ def del_cat(request, cat_id):
         return HttpResponseRedirect('/perm_error/')
 
     Category.objects.get(id=cat_id).delete()
+    
+    # Update the category count
+    corpus.cat_count = Category.objects.filter(corpus=corpus).count()
+    corpus.save()
+    
     
     return HttpResponseRedirect('/corpus/'+str(corpus.id))
 
